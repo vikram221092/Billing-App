bb589ad9eaa92925d398b4c29a46824f0a655ff0
vikram <vikramscvikram@gmail.com>
2021-10-31 15:50:30 +0530

    Cart item added

--- src/components/Bills/CartItem.js
+++ src/components/Bills/CartItem.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import { TableRow , TableCell , Button } from '@material-ui/core'
 
-const CartItem = () => {
+const CartItem = ({id,quantity,product,products,decrementQuantity,incrementQuantity,removeItem}) => {
+   
+    const getProduct=()=>{
+        if(product.length!==0){
+           const result=products.find((ele)=>{
+               return ele._id===product
+           })
+           return result
+       }
+     }
+
+     const handleIncrement=(id)=>{
+        incrementQuantity(id)
+     }
+     const handleDecrement=(id)=>{
+        decrementQuantity(id)
+     }
+     const removeHandle=(id)=>{
+        removeItem(id)
+     }
+
     return (
-        <div>
-            
-        </div>
+        <>
+        <TableRow>
+                <TableCell>
+                    {getProduct(product).name}
+                </TableCell>
+                <TableCell>
+                {getProduct(product).price * Number(quantity)}
+                </TableCell>
+                <TableCell>     
+                        <button disabled={quantity <=1} style={{margin:'3px'}} onClick={()=>handleDecrement(id)} > - </button> 
+                        {quantity} 
+                        <button style={{margin:'2px'}} onClick={()=>handleIncrement(id)} > + </button>
+                </TableCell>
+                <TableCell>
+                        <Button onClick={()=>removeHandle(id)}>Remove</Button>
+                </TableCell>
+        </TableRow>
+    </>
     )
 }
 
